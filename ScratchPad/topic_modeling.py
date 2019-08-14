00edda56d2c31a69a9b6093cf57a642963d76025
Jiseong Michael Yang <jsyang.michael@gmail.com>
2019-08-14 23:54:52 +0900

    topic modeling joint probability

---
+++ ScratchPad/topic_modeling.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+w_t = np.array([0.231, 0.313, 0, 0.312])
+w_t.reshape(2,2)
+
+d_t = np.array([0.375, 0.625, 0, 0.375])
+d_t.reshape(2,2)
+
+w_dn = w_t * d_t
+w_dn.reshape(2,2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
